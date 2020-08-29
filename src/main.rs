f728eff99027795f1469b319cbb3a603fc71d947
YohananDiamond <mitonanan12@gmail.com>
2020-08-29 19:56:40 -0300

    Cleanup?

--- src/main.rs
+++ src/main.rs
@@ -15,20 +15,10 @@ use simplelog::{LevelFilter, SimpleLogger};
 mod layouts;
 mod misc;
 
-use penrose::draw::*;
-use std::{thread, time};
-
 fn main() -> penrose::Result<()> {
-    // penrose will log useful information about the current state of the WindowManager during
-    // normal operation that can be used to drive scripts and related programs. Additional debug
-    // output can be helpful if you are hitting issues.
     SimpleLogger::init(LevelFilter::Debug, simplelog::Config::default()).unwrap();
 
-    // Config structs can be intiialised directly as all fields are public.
-    // A default config is provided which sets sensible (but minimal) values for each field.
     let mut config = Config::default();
-
-    // Created at startup. See keybindings below for how to access them
     config.workspaces = vec!["1", "2", "3", "4", "5", "6", "7", "8", "9"];
 
     // Windows with a matching WM_CLASS will always float
@@ -61,21 +51,8 @@ fn main() -> penrose::Result<()> {
         // Layout::floating("[----]"),
     ];
 
-    // Gaps
     config.gap_px = 0;
 
-    // Here we are using a contrib hook that requires configuration to set up a default workspace
-    // on workspace "9". This will set the layout and spawn the supplied programs if we make
-    // workspace "9" active while it has no clients.
-    // config.hooks.push(DefaultWorkspace::new(
-    //     "9",
-    //     "[botm]",
-    //     vec![my_terminal, my_terminal, my_file_manager],
-    // ));
-
-    // Scratchpad is an extension: it makes use of the same Hook points as the examples above but
-    // addtionally provides a 'toggle' method that can be bound to a key combination in order to
-    // trigger the bound scratchpad client.
     let sp = Scratchpad::new("st", 0.8, 0.8);
     sp.register(&mut config);
 
@@ -143,53 +120,6 @@ fn main() -> penrose::Result<()> {
     // event loop. From this point on, program control passes to the WindowManager so make sure
     // that any logic you wish to run is done before here!
     wm.grab_keys_and_run(key_bindings);
-
-    Ok(())
-}
-
-const HEIGHT: usize = 18;
-
-const BLACK: u32 = 0x282828;
-const GREY: u32 = 0x3c3836;
-const WHITE: u32 = 0xebdbb2;
-const PURPLE: u32 = 0xb16286;
-const BLUE: u32 = 0x458588;
-const RED: u32 = 0xcc241d;
-
-fn start_bar() -> penrose::Result<()> {
-    let workspaces = &["1", "2", "3", "4", "5", "6"];
-    let style = TextStyle {
-        font: "JetBrains Mono Medium".to_string(),
-        point_size: 11,
-        fg: WHITE.into(),
-        bg: Some(BLACK.into()),
-        padding: (2.0, 2.0),
-    };
-    let highlight = BLUE;
-    let empty_ws = GREY;
-    let mut bar = dwm_bar(
-        Box::new(XCBDraw::new()?),
-        0,
-        HEIGHT,
-        &style,
-        highlight,
-        empty_ws,
-        workspaces,
-    )?;
-
-    let config = Config::default();
-    let conn = XcbConnection::new().unwrap();
-    let mut wm = WindowManager::init(config, &conn);
-
-    thread::sleep(time::Duration::from_millis(1000));
-    for focused in 1..6 {
-        bar.workspace_change(&mut wm, focused - 1, focused);
-        bar.event_handled(&mut wm);
-        thread::sleep(time::Duration::from_millis(1000));
-    }
-
-    // lmao
-    thread::sleep(time::Duration::from_millis(99999999999999999));
 
     Ok(())
 }
